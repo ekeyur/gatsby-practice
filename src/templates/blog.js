7c200c9f554ef45d688b042f98ea0b39626cb881
Keyur Patel <ekeyur@gmail.com>
2019-06-20 19:37:57 -0400

    changed blogs data from markdown to contentful apis

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import Layout from '../components/layout';
 import { graphql } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
+import Layout from '../components/layout';
 export const query = graphql`
     query (
       $slug:String!
     ) {
-      markdownRemark (
-        fields: {
+      contentfulBlogPost (
+       
           slug: {
             eq: $slug
           }
-        }
+        
       ) {
-        frontmatter {
+        
           title
-          date
-        }
-        html
+          publishedDate(formatString: "MMMM Do, YYYY")
+          body {
+            json
+          }
       }
     }
     `
@@ -25,11 +27,11 @@ export const query = graphql`
 const Blog = (props) => {
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{ __html : props.data.markdownRemark.html}}>
-
-      </div>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.publishedDate}</p>
+      {documentToReactComponents(props.data.contentfulBlogPost.body.json)}
+      {/* <div dangerouslySetInnerHTML={{ __html : props.data.markdownRemark.html}}>
+      </div> */}
     </Layout>
   )
 }
