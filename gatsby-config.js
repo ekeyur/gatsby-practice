28b587e46d5f8bfa810119609244cf93d2d84900
Keyur Patel <ekeyur@gmail.com>
2019-06-20 14:59:17 -0400

    created blog templates

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,24 @@
  * Configure your Gatsby site with this file.
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+ **/
 
 module.exports = {
   siteMetadata: {
     title: 'Full-Stack Bootcamp!',
-    author: 'Keyur Patel'
+    author: 'Keyur Y. Patel'
   },
+
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-transformer-remark',
   ]
 }
+
