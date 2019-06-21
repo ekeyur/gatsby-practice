491c33fb9a8334af8c6e86fcabd23803c3eea1d2
Keyur Patel <ekeyur@gmail.com>
2019-06-20 20:16:46 -0400

    added siteMetadata and title

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import Head from '../components/head';
 
 const AboutPage = () => {
   return (
     <Layout>
+      <Head title="About" />
       <h1>About Me</h1>
       <p> I am currently in between jobs</p>
       <p> Want to work with me? Reach out <Link to="/contact">Contact me.</Link></p>
